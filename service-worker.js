a6e9e456a8f7268767c784706cea1f29aa03af82
AndSuch-Mods <michaelcgrace18@gmail.com>
2026-08-12 19:24:04 -0500

    Refresh offline cache for parts update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='cm2109-checklist-v3';
+const CACHE='cm2109-checklist-v4';
 const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
